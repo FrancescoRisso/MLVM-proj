407a03e68cffbf565687a5420b9b89e4121a7230
FrancescoRisso <francesco@frisso.net>
2025-05-13 19:28:33 +0200

    Fix main not working

--- main.py
+++ main.py
@@ -8,8 +8,8 @@ from settings import Settings
 
 
 def main():
-    dataset = DataSet(Split.TRAIN, (15, 20))
-    midi, (sample_rate, audio) = dataset[-1]
+    dataset = DataSet(Split.TRAIN, Settings.seconds)
+    midi, audio = dataset[-1]
 
     net = HarmonicCNN().to(Settings.device)
     (yo, yp, yn), out_midi = net(audio)
