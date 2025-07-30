2b7312ff6dc800451051b28bf7a9259d2c1e898e
Samuele Tallone <s334046@studenti.polito.it>
2025-07-30 16:41:54 +0200

    eval plots 5 best and 5 worst with gt

--- main.py
+++ main.py
@@ -1,9 +1,10 @@
+from train.train import evaluate_and_plot_extremes
+from dataloader.split import Split
 from train.train import train
 
-
 def main():
-	
-    train()
+    #train()
+    evaluate_and_plot_extremes("model_saves/best_model_40.pth", Split.VALIDATION)
 
 
 if __name__ == "__main__":
