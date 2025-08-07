3161635c87398630b24f96678663f09a7dbed8d5
FrancescoRisso <francesco@frisso.net>
2025-08-07 12:16:41 +0200

    Better typing

--- train/inference.py
+++ train/inference.py
@@ -1,38 +1,40 @@
-import pretty_midi
-import torch
 import librosa
+import pretty_midi  # type: ignore
+import torch
+from mido import MidiFile  # type: ignore
+
 from dataloader.Song import Song
 from model.model import HarmonicCNN
+from model.postprocessing import postprocess
 from model_rnn.model import HarmonicRNN
 from settings import Model
 from settings import Settings as s
-from model.postprocessing import postprocess
 
 
 def inference(
-        audio_path: str,
-        return_path: bool | str = False,
-        output_path: str = "trial_audio/output.mid",
-) -> pretty_midi.PrettyMIDI | str:
-    
+    audio_path: str,
+    write_to_file: bool = False,
+    output_path: str = "trial_audio/output.mid",
+) -> pretty_midi.PrettyMIDI | MidiFile | str:
+
     device = s.device
     print(f"Evaluating on {device}")
-        
+
     model = (HarmonicCNN() if s.model == Model.CNN else HarmonicRNN()).to(device)
     if s.pre_trained_model_path is not None:
         model.load_state_dict(torch.load(s.pre_trained_model_path, map_location=device))
         print(f"Loaded pre-trained model from {s.pre_trained_model_path}")
-    
+
     model.eval()
 
-    audio, _ = librosa.load(  # type: ignore
-            audio_path,
-            sr=s.sample_rate,
-        )
-    audio = torch.tensor(audio).unsqueeze(0)
+    audio_np, _ = librosa.load(  # type: ignore
+        audio_path,
+        sr=s.sample_rate,
+    )
+    audio = torch.tensor(audio_np).unsqueeze(0)
     audio = audio.to(device)
 
-    if model is HarmonicRNN:
+    if isinstance(model, HarmonicRNN):
         audio_input = audio.reshape((1, -1, s.sample_rate))
         pred_midi, pred_len, pred_tpb = model(torch.Tensor(audio_input))
         midi = Song.from_np(
@@ -46,19 +48,25 @@ def inference(
         with torch.no_grad():
             # Assume audio is already preprocessed and ready for model input
             yo_pred, yp_pred, yn_pred = model(audio)
-            
+
             yo_pred = torch.sigmoid(yo_pred)
             yp_pred = torch.sigmoid(yp_pred)
             yn_pred = torch.sigmoid(yn_pred) if s.remove_yn == False else yp_pred
 
-            yo_pred = yo_pred.squeeze(1) 
+            yo_pred = yo_pred.squeeze(1)
             yp_pred = yp_pred.squeeze(1)
             yn_pred = yn_pred.squeeze(1) if s.remove_yn == False else yp_pred
 
-            midi = postprocess(yo_pred, yp_pred, yn_pred, audio_length=audio.shape[-1], sample_rate=s.sample_rate)
+            midi = postprocess(
+                yo_pred,
+                yp_pred,
+                yn_pred,
+                audio_length=audio.shape[-1],
+                sample_rate=s.sample_rate,
+            )
 
-        if return_path:
-            midi.write(output_path)
-            return output_path
-        else:
-            return midi
+    if write_to_file:
+        midi.write(output_path)  # type: ignore
+        return output_path
+    else:
+        return midi
